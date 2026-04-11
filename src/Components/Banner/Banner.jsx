1499fc1317ea20cf7a08967adf0b9fcc3a163d78
Musfiq009 <musfiq.rahat19@gmail.com>
2026-04-11 13:35:43 +0600

    Added Banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,38 @@
-
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="max-w-7xl mx-auto py-16">
+      <div className="flex gap-16 justify-center items-center">
+        <div className="space-y-8">
+          <div className="space-y-4">
+            <div className="badge badge-soft badge-primary px-4 py-3 rounded-full">
+              <img src="/src/assets/Bullet.png" alt="bullet point" />
+              New: AI-Powered Tools Available
+            </div>
+            <h1 className="text-7xl font-extrabold text-[#101727] leading-24">
+              Supercharge Your <br />
+              Digital Workflow
+            </h1>
+            <p className="text-lg text-[#627382] leading-7 max-w-2xl">
+              Access premium AI tools, design assets, templates, and
+              productivity software—all in one place. Start creating faster
+              today. <br /> Explore Products
+            </p>
+          </div>
+          <div className="flex gap-4">
+            <button className="btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-amber-50 font-bold">
+              Explore Products
+            </button>
+            <button className="btn btn-outline btn-primary rounded-full font-bold">
+              <img src="/src/assets/Play.png" alt="play icon" /> Watch Demo
+            </button>
+          </div>
         </div>
-    );
+        <div>
+            <img src="/src/assets/banner.png" alt="Banner" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
